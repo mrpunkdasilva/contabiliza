b611c2b85a7ff785754462dd246e7590b200d251
Mr Punk da Silva <gustavojs417@gmail.com>
2025-07-01 17:15:26 -0300

    feat: add icons interface

---
+++ src/components/icons-interface/index.js
@@ -0,0 +1,9 @@
+import UserIcon from './UserIcon.vue';
+import KeyRoundIcon from './KeyRoundIcon.vue';
+import MailIcon from './MailIcon.vue';
+
+export {
+  UserIcon,
+  KeyRoundIcon,
+  MailIcon,
+};
