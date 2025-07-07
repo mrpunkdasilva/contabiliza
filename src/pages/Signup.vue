17f5237af9720a4169454792e912bce6ca540f20
Mr Punk da Silva <gustavojs417@gmail.com>
2025-07-06 21:02:04 -0300

    feat: refactor signup to use authenticationlayout

--- src/pages/Signup.vue
+++ src/pages/Signup.vue
@@ -1,101 +1,59 @@
 <template>
-  <main>
-    <img :src="ContabilizaBrand" alt="Contabiliza Brand"/>
-
-    <form>
-      <fieldset class="input-fields">
-        <div>
-          <label class="sr-only" for="username">
-            Username
-          </label>
-          <InputComponent
-              placeholder="Username"
-              type="text"
-              id="username"
-              name="username"
-              required
-          />
-        </div>
-
-        <label class="sr-only" for="email">
-          Email
+  <AuthenticationLayout>
+    <fieldset class="input-fields">
+      <div>
+        <label class="sr-only" for="username">
+          Username
         </label>
         <InputComponent
-            placeholder="Email"
-            type="email"
-            id="email"
-            name="email"
+            placeholder="Username"
+            type="text"
+            id="username"
+            name="username"
             required
         />
+      </div>
 
-        <div>
-          <label class="sr-only" for="password">
-            Password
-          </label>
-          <InputComponent
-              placeholder="Password"
-              type="password"
-              id="password"
-              name="password"
-              required
-          />
-        </div>
-      </fieldset>
+      <label class="sr-only" for="email">
+        Email
+      </label>
+      <InputComponent
+          placeholder="Email"
+          type="email"
+          id="email"
+          name="email"
+          required
+      />
 
-      <fieldset class="buttons-actions">
-        <button class="button-primary">Sign up</button>
-        <button class="button-secondary">Login</button>
-      </fieldset>
-    </form>
-
-  </main>
+      <div>
+        <label class="sr-only" for="password">
+          Password
+        </label>
+        <InputComponent
+            placeholder="Password"
+            type="password"
+            id="password"
+            name="password"
+            required
+        />
+      </div>
+    </fieldset>
+  </AuthenticationLayout>
 </template>
 
 <script>
-import ContabilizaBrand from '../assets/img/contabiliza.branding.png';
 import InputComponent from "../components/Forms/InputComponent.vue";
+import AuthenticationLayout from "../Layouts/AuthenticationLayout.vue";
 
 export default {
   name: 'Signup',
-  components: {InputComponent},
+  components: {AuthenticationLayout, InputComponent},
   data() {
-    return {
-      ContabilizaBrand,
-    };
+    return {};
   },
 
 };
 </script>
 
 <style scoped lang="scss">
-@use "../variables" as *;
-
-main {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: $spacing-6;
-
-  form {
-    display: flex;
-    flex-direction: column;
-    gap: $spacing-6;
-
-    fieldset {
-      border: none;
-
-      &.input-fields {
-        display: flex;
-        flex-direction: column;
-        gap: $spacing-4;
-      }
-
-      &.buttons-actions {
-        display: flex;
-        gap: $spacing-6;
-      }
-    }
-  }
-}
 </style>
