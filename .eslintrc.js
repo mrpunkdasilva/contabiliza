08f6498a3ea55febed0fc997ab2949f9cad89944
Mr Punk da Silva <gustavojs417@gmail.com>
2025-08-11 23:40:11 -0300

    refactor: Remove all TypeScript-related configurations and files
    - Removed TypeScript dependencies and scripts from package.json.
    - Cleaned up ESLint configurations (.eslintrc.js and eslint.config.js) to remove TypeScript-specific rules.
    - Replaced jest.config.ts with a JavaScript-compatible jest.config.js.
    - Ensured no TypeScript files remain in the project.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,9 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
   ],
   rules: {
-    // Suas regras customizadas aqui
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-explicit-any": "off",
+    // Your custom rules here
   },
 };
--- eslint.config.js
+++ eslint.config.js
@@ -2,15 +2,13 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-
-export default tseslint.config([
+export default [
   {
     ignores: ["dist/**/*", "node_modules/**/*"],
   },
   {
-    files: ["**/*.{ts,tsx,js,jsx}"],
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{js,jsx}"],
+    extends: [js.configs.recommended],
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
